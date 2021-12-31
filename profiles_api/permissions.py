83e8f60e04b91ec3726a56b1a401a686bc245b67
thejineaswar <thejineash@gmail.com>
2021-12-31 20:04:26 +0530

    Added profiles ViewSet

---
+++ profiles_api/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework import permissions
+
+
+class UpdateOwnProfile(permissions.BasePermission):
+    """ Allow user to edit their own profile """
+
+    def has_object_permission(self, request, view, obj):
+        """ Check if the user is trying to edit their own profile"""
+        if request.method in permissions.SAFE_METHODS: #Safe methods normally covers method which doesnt make changes
+                                                          # to the data which is present in the database
+            return True
+
+        return obj.id == request.user.id #Checking if the object to which the changes are made, belong to the user
