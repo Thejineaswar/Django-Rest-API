f171be6a048c07485567430616e58a5d2abf3f3e
thejineaswar <thejineash@gmail.com>
2022-01-02 12:42:18 +0530

    Getting rid of secret key

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from .secrets import Secrets
+secret_class = Secrets()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*=6=ubkc4t84(@l*r=@#ybdvedmr!37idd$yvvrfl7*&4zr=-='
+SECRET_KEY = secret_class.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG',1)))
